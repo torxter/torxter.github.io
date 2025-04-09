bf9c24271d584f542305f5d6513c767c85e6f2cd
Thomas McCavour <194552106+torxter@users.noreply.github.com>
2025-04-09 13:06:37 -0400

    Styles
    New stylesheet, random selection of stylesheet

---
+++ script.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setStylesheet() {
+
+    var style = "style" + getRandomInt(1, 2) + ".css";
+
+    document.getElementById("stylesheet").setAttribute("href", style);
+}
